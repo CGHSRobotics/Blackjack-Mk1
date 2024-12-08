b83eb8a07c0602f8d7f9f9ce0b4fd0d9c81bf913
Renblas <renblas.dev@gmail.com>
2024-12-08 11:24:41 -0500

    t

---
+++ src/lvgl.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+namespace ace::lvgl {
+
+
+
+
+
+
+
+
+
+}  // namespace ace::lvgl
--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,140 @@
 #include "main.h"
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
+#include "util.h"
+
+/* ========================================================================== */
+/*                                    Ports                                   */
+/* ========================================================================== */
+#define CHASSIS_L_1 -11
+#define CHASSIS_L_2 -2
+#define CHASSIS_L_3 -1
+#define CHASSIS_R_1 20
+#define CHASSIS_R_2 19
+#define CHASSIS_R_3 12
+#define OPTICAL_PORT 5
+
+/* ========================================================================== */
+/*                                   Devices                                  */
+/* ========================================================================== */
+
+// Controller
+Controller controller;
+// This may need to go into init!
+pros::Optical optical_sensor(OPTICAL_PORT);
+// Controller Task
+/*
+blkjack::ControllerText controllerText(&controller);
+pros::Task update_controller_task = pros::Task(blkjack::update_controller_text, (void*)&controllerText);
+*/
+
+// Chassis Controller - lets us drive the robot around with open- or closed-loop control
+std::shared_ptr<ChassisController> drive = ChassisControllerBuilder()
+											   .withMotors(MotorGroup({CHASSIS_L_1, CHASSIS_L_2, CHASSIS_L_3}), MotorGroup({CHASSIS_R_1, CHASSIS_R_2, CHASSIS_R_3}))
+											   // Green gearset, 4 in wheel diam, 11.5 in wheel track
+											   .withDimensions(AbstractMotor::gearset::blue, {{2.75_in, 9_in}, imev5BlueTPR})
+											   .withOdometry()
+											   .build();
+
+// Arm related objects
+ADIButton armLimitSwitch('H');
+
+// other motors, pistons, sensors etc
+Motor armMotor(-8, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
+MotorGroup intakeMotors({-15, 6});
+
+/* ========================================================================== */
+/*                           Include Auton And LVGL                           */
+/* ========================================================================== */
+
+#include "auton.h"
+#include "lvgl.h"
+#include "notagame.h"
+
+/* ========================================================================== */
+/*                                 Initialize                                 */
+/* ========================================================================== */
+void initialize()
+{
+}
+
+/* ========================================================================== */
+/*                                  Disabled                                  */
+/* ========================================================================== */
+void disabled() {}
+
+/* ========================================================================== */
+/*                           Competition Initialize                           */
+/* ========================================================================== */
+void competition_initialize() {}
+
+/* ========================================================================== */
+/*                                 Autonomous                                 */
+/* ========================================================================== */
+void autonomous()
+{
+
+	// Exception conditions, should never trigger
+	if (auton::cur_auton < 0)
+		auton::cur_auton = 0;
+	if (auton::cur_auton >= auton::NUM_AUTONS)
+		auton::cur_auton = auton::NUM_AUTONS - 1;
+
+	// Switch case
+	switch (str2int(auton::autons_arr[auton::cur_auton]))
+	{
+	case str2int("red side"):
+		auton::red_side();
+		break;
+
+	case str2int("blue side"):
+
+		break;
+
+	case str2int("skills"):
+
+		break;
+
+	default:
+
+		break;
 	}
 }
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+/* ========================================================================== */
+/*                                User Control                                */
+/* ========================================================================== */
+void opcontrol()
+{
+	pros::Optical optical_sensor(OPTICAL_PORT);
+	
 
-	pros::lcd::register_btn1_cb(on_center_button);
-}
+	blkjack::MotorButton armButton(&armMotor, ControllerDigital::A);
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
+	blkjack::MotorButton intakeButton(&intakeMotors, ControllerDigital::L1);
+	// blkjack::MotorButton intakeButton(&intakeMotors, ControllerDigital::B);
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
+	ControllerButton notagamebutton(ControllerDigital::X);
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
+	while (true)
+	{
+		// if (notagamebutton.changedToPressed()) not_blackjack(&controllerText);
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+		/* ---------------------------------- Drive --------------------------------- */
+		// arcade drive with left and right sticks
+		drive->getModel()->tank(controller.getAnalog(ControllerAnalog::leftY),
+								controller.getAnalog(ControllerAnalog::rightY));
 
+		/* ------------------------- Other Motors / Pistons ------------------------- */
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+		armButton.toggle();
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		intakeButton.press();
+
+		// intakeButton.toggle();
+
+		/* ---------------------------------- Delay --------------------------------- */
+		// Wait and give up the time we don't need to other tasks.
+		// Additionally, joystick values, motor telemetry, etc. all updates every 10 ms.
+		pros::delay(10);
 	}
 }
