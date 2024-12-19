295e9d8ec4aafa23602dad10c345402b4dd8e1cd
CGHSRobotics <cghsrobotics@gmail.com>
2024-12-19 11:25:46 -0500

    removed explicit labels :)

--- src/lvgl.cpp
+++ src/lvgl.cpp
@@ -21,7 +21,7 @@ void lv_example_get_started_3(void)
 	lv_obj_t* label2 = lv_label_create(lv_scr_act());
 	lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL_CIRCULAR);     /*Circular scroll*/
 	lv_obj_set_width(label2, 150);
-	lv_label_set_text(label2, "Lydia is a faggggggggggggggggggot");
+	lv_label_set_text(label2, "code worky");
 	lv_obj_align(label2, LV_ALIGN_CENTER, 0, 40);
 }
 
